11955d66946cf5ff0b46a22bc367511756431f24
Anurag_kushwaha <kushwahaanurag504@gmail.com>
2025-05-11 19:55:28 +0530

    remove sentri

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Approach from "@/components/Approach";
+
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
